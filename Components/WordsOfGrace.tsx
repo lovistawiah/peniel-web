8b7651ee7288a6e901ea9af6aba8be754fea55c6
Lovis Tawiah <lovistawiah638@gmail.com>
2025-06-27 03:28:59 +0000

    Add words of grace

---
+++ Components/WordsOfGrace.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const WordsOfGrace = () => {
+  return (
+    <div className='w-[90%] h-fit self-center bg-transparent rounded-[20px] p-[20px] text-white border border-[#454545] text-center'>
+        <h1 className='text-2xl leading-1.5'>Words of <span className='text-4xl text-[#ebbd48] font-potta'>Grace</span>
+        </h1>
+        <p className='pt-[10px] opacity-50'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere doloribus corporis sunt nulla unde id modi illo eligendi, accusantium itaque ratione harum temporibus? Odio eum corporis expedita officia culpa iure tempora dignissimos eos dolor soluta sunt voluptates numquam itaque exercitationem quia enim deleniti maiores ad, ratione a hic laboriosam tempore molestiae quasi? Delectus consequatur ipsum modi non! <span className='text-[#ebbd48] opacity-100'>Magni ratione quis ut delectus,</span> laudantium quia eveniet in eos laboriosam excepturi tenetur blanditiis magnam doloribus ipsam numquam, id quasi sed dolor sapiente iste soluta cumque nam provident. Quisquam odit quibusdam quo rem ab molestiae natus tempora debitis mollitia laborum beatae illo ducimus, expedita at asperiores dolorum vero sapiente blanditiis possimus amet doloribus enim. <span className='text-[#0970b1]'>Non molestias labore corporis dignissimos placeat nobis commodi ullam.</span></p>
+    </div>
+  )
+}
+
+export default WordsOfGrace
