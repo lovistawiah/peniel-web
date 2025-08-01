990b425ed5f8584b5498f440085a20492d7fb6d3
Lovis Tawiah <lovistawiah638@gmail.com>
2025-08-01 16:20:05 +0000

    Add sublinks functionality

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -1,9 +1,14 @@
+'use client'
 import {CiMenuBurger,CiLocationOn,} from 'react-icons/ci'
 import {FaChevronDown,FaInstagram,FaFacebookF,FaPhone,FaEnvelope} from 'react-icons/fa6'
 import {FiYoutube,} from 'react-icons/fi'
 import Link from 'next/link'    
 import Image from 'next/image'
+import useCloseSublinks from '@/hooks/useCloseSublinks'
 const Navbar = () => {
+
+    const { toggleAbout, openAbout, toggleMedia, openMedia } = useCloseSublinks()
+
   return (
     <section className='bg-[#111111] w-full  font-potta text-white'>
         <nav>
@@ -26,12 +31,12 @@ const Navbar = () => {
         <ul className='flex flex-col items-center justify-center text-white h-fit w-full font-Potta [&_li]:text-center [&_li]:text-[20px] [&_li]:border-b-[0.5px] [&_li]:border-b-[#454545] [&_li]:w-full [&_li]:h-[70px] [&_li]:flex [&_li]:items-center [&_li]:justify-center [&>li>p]:flex [&>li>p]:justify-center [&>li>p]:items-center [&>li>p]:gap-[4px] [&>li>p]:underline  [&_li_a]:flex [&_li_a]:justify-center [&_li_a]:items-center [&_li]:cursor-pointer [&_li_a]:gap-1 [&_li_a]:w-full [&_li_a]:h-full [&_li_a]:underline first:border-t-[0.5px] '>
         
             {/* about us */}
-            <li>
+            <li onClick={toggleAbout} className='relative'>
                 <p>About Us <FaChevronDown /></p>
             </li>
 
             {/* sub links to the about page */}
-            <section className='w-full h-fit'>
+            <section className={`w-full h-fit ${openAbout ? 'block' : 'hidden'}`}>
             <ul className=' w-full h-fit [&_li]:bg-[#1C1C1C]'>
                 <li>
                     <Link href={"/about"}>Who We Are</Link>
@@ -59,7 +64,24 @@ const Navbar = () => {
             </li>
 
             {/* Media*/}
-
+        <li onClick={toggleMedia} className='relative'>
+                <p>Media <FaChevronDown /></p>
+            </li>
+            
+            {/* sub links to the about page */}
+            <section className={`w-full h-fit ${openMedia ? 'block' : 'hidden'}`}>
+            <ul className=' w-full h-fit [&_li]:bg-[#1C1C1C]'>
+                <li>
+                    <Link href={"/about"}>Live</Link>
+                </li>
+                <li>
+                    <Link href={"/"}>Online Radio</Link>
+                </li>
+                <li>
+                    <Link href={"/"}>Sermons</Link>
+                </li>
+            </ul>
+            </section>
 
             {/* Departments */}
             <li >
--- hooks/useCloseSublinks.ts
+++ hooks/useCloseSublinks.ts
@@ -1,5 +1,28 @@
-const useCloseSublinks = () => {
+import { useState, } from "react"
 
+const useCloseSublinks = () => {
+    const [openAbout, setOpenAbout] = useState(false)
+    const [openMedia, setOpenMedia] = useState(false)
+
+    const toggleAbout = () => {
+        if (openAbout) {
+            setOpenAbout(false)
+
+        } else {
+            setOpenAbout(true)
+            setOpenMedia(false) // Close Media sublinks when About is opened
+        }
+    }
+    const toggleMedia = () => {
+        if (openMedia) {
+            setOpenMedia(false)
+
+        } else {
+            setOpenMedia(true)
+            setOpenAbout(false) // Close About sublinks when Media is opened
+        }
+    }
+    return { toggleAbout, openAbout, toggleMedia, openMedia }
 }
 
 export default useCloseSublinks
